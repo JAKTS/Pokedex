c0e50626a828fa9c03dcc7f13a8262dcdc2006c1
Michael Davis <endodavis@gmail.com>
2016-07-15 10:20:30 -0600

    Updating to close

--- js/pokeCtrl.js
+++ js/pokeCtrl.js
@@ -2,7 +2,14 @@ angular.module('pokedex')
   .controller('pokeCtrl', function ($scope, pokeServ) {
 
 //manipulate api data here
-
+    $scope.pokeData = function(num){
+      pokeServ.getPokemon(num).then(function (response){
+        console.log(response);
+      $scope.pokepic = response.sprites.front_shiny;
+      $scope.moves = response.moves[0].move.name;
+      console.log($scope.moves);
+      });
+    };
 
 
   });
--- js/pokeServ.js
+++ js/pokeServ.js
@@ -1,7 +1,22 @@
 angular.module('pokedex')
-  .service('pokeServ', function () {
-    //make api call to 
+  .service('pokeServ', function ($http) {
+    //make api call to
+    this.getPokemon = function(num) {
+      return $http({
+        method: 'GET',
+        url: 'http://pokeapi.co/api/v2/pokemon/' + num + '/'
 
+      }).then(function(response){
+        console.log(response);
+        return response.data;
+      });
+    };
 
 
   });
+
+//POST, GET, PUT, DELETE
+//Create
+//Read
+//Update
+//Delete
